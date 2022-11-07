f7d94a2c7ee57e9bd54570f5be8690970002cad2
Carlos00-pixel <carlos.mayoral00@gmail.com>
2022-11-07 14:28:17 +0100

    commit

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -2,12 +2,25 @@
   <div style="margin-top: 20px">
     <h1>PAGINA PRINCIPAL</h1>
     <img src="./../assets/logo.png"/>
+    <h2 style="color: blue">{{saludo}}</h2>
   </div>
 </template>
 
 <script>
+import ServiceEjemplo from './../services/ServiceEjemplo';
+//PARA PODER UTILIZAR NUESTROS SERVICIO, DEBEMOS INSTANCIAR UN OBJETO DE LA CLASE EN EL COMPONENTE
+const service = new ServiceEjemplo();
+
 export default {
-    name: "HomeComponent"
+    name: "HomeComponent",
+    data(){
+      return {
+        saludo: ""
+      }
+    },
+    mounted(){
+      this.saludo = service.getSaludo("Carlos");
+    }
 }
 </script>
 
